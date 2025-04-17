63d91c50a6cbb510df33e3f2488cbd4b4f5b5483
felipesora <felipeusora@gmail.com>
2025-04-17 14:32:34 -0300

    Criando o Entity de Departamento

---
+++ src/main/java/br/com/fiap/mototrack_backend_java/model/Departamento.java
@@ -0,0 +1,47 @@
+package br.com.fiap.mototrack_backend_java.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "mt_departamentos")
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @OneToMany(mappedBy = "departamento")
+    private List<Movimentacao> movimentacoes;
+
+    public Departamento() {
+    }
+
+    public Departamento(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Movimentacao> getMovimentacoes() {
+        return movimentacoes;
+    }
+}
