149442e59bc000803a5cbbeb6aec13c0d9b7ea7e
felipesora <felipeusora@gmail.com>
2025-04-21 13:17:19 -0300

    Criando MovimentacaoDTO

---
+++ src/main/java/br/com/fiap/mototrack_backend_java/dto/MovimentacaoDTO.java
@@ -0,0 +1,31 @@
+package br.com.fiap.mototrack_backend_java.dto;
+
+public class MovimentacaoDTO {
+    private Long id;
+    private LocalDateTime timestamp;
+    private DepartamentoDTO departamento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public DepartamentoDTO getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(DepartamentoDTO departamento) {
+        this.departamento = departamento;
+    }
+}
