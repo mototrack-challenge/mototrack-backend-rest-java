59995d3ca3e2a7285ab4967af77d2934133ea2af
felipesora <felipeusora@gmail.com>
2025-04-21 13:17:58 -0300

    Criando DepartamentoDTO

---
+++ src/main/java/br/com/fiap/mototrack_backend_java/dto/DepartamentoDTO.java
@@ -0,0 +1,31 @@
+package br.com.fiap.mototrack_backend_java.dto;
+
+public class DepartamentoDTO {
+    private Long id;
+    private String nome;
+    private String tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
