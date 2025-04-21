44c7798df6b9df2405c80e47245076b80f609c29
felipesora <felipeusora@gmail.com>
2025-04-21 13:16:21 -0300

    Criando StatusDTO

---
+++ src/main/java/br/com/fiap/mototrack_backend_java/dto/StatusDTO.java
@@ -0,0 +1,31 @@
+package br.com.fiap.mototrack_backend_java.dto;
+
+public class StatusDTO {
+    private Long id;
+    private String descricao;
+    private String tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
