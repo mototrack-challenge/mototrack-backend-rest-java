55edc9ac22f0c12910b7b43fdaa89bba007f3491
felipesora <felipeusora@gmail.com>
2025-04-30 15:20:05 -0300

    Criando UsuarioDTO com Bean Validation e ajustando UsuarioController

--- src/main/java/br/com/fiap/mototrack_backend_java/controller/UsuarioController.java
+++ src/main/java/br/com/fiap/mototrack_backend_java/controller/UsuarioController.java
@@ -1,15 +1,50 @@
 package br.com.fiap.mototrack_backend_java.controller;
 
+import br.com.fiap.mototrack_backend_java.dto.UsuarioDTO;
+import br.com.fiap.mototrack_backend_java.mapper.UsuarioMapper;
 import br.com.fiap.mototrack_backend_java.model.Usuario;
 import br.com.fiap.mototrack_backend_java.service.UsuarioService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/usuarios")
-public class UsuarioController extends BaseController<Usuario, Long> {
+public class UsuarioController {
+
+    private final UsuarioService service;
 
     public UsuarioController(UsuarioService service) {
-        super(service);
+        this.service = service;
+    }
+
+    @GetMapping("/listar/todos")
+    public List<UsuarioDTO> listarTodos() {
+        return service.listarTodos()
+                .stream()
+                .map(UsuarioMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/listar/{id}")
+    public UsuarioDTO buscarPorId(@PathVariable Long id) {
+        return UsuarioMapper.toDTO(service.buscarPorId(id));
+    }
+
+    @PostMapping("/salvar")
+    public UsuarioDTO salvar(@RequestBody @Valid UsuarioDTO dto) {
+        return UsuarioMapper.toDTO(service.salvar(UsuarioMapper.toEntity(dto)));
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public UsuarioDTO atualizar(@PathVariable Long id, @RequestBody @Valid UsuarioDTO dto) {
+        return UsuarioMapper.toDTO(service.atualizar(id, UsuarioMapper.toEntity(dto)));
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public String deletar(@PathVariable Long id) {
+        return service.deletar(id);
     }
 }
